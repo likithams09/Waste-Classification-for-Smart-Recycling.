5e9d8b9841e90e08d59475d170f9afeabf81627d
likithams09 <mlikithaBTECH23@ced.alliance.edu.in>
2026-03-08 22:57:55 +0530

    main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
+import tensorflow as tf
+import numpy as np
+import cv2
 
+model = tf.keras.models.load_model("waste_model.h5")
+
+classes = ["cardboard","glass","metal","paper","plastic","trash","battery","organic","e-waste"]
+
+img = cv2.imread("test.jpg")
+img = cv2.resize(img,(224,224))
+img = img/255.0
+img = np.reshape(img,(1,224,224,3))
+
+prediction = model.predict(img)
+
+print("Predicted Class:",classes[np.argmax(prediction)])
+print("Confidence:",np.max(prediction))
